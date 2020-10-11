6fe7e0d153727f05a97438c3ba70cea81bdf8cc3
Vyacheslav / Local <vyach.kotlyarov@gmail.com>
2020-10-11 12:01:56 +0300

    3 commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # jQuery plugin for collecting elements
 Plugin for animating elements with the random effect.
 
-By default: ([DEMO](https://kotlyarov.us/jquery-mosaic-elements/))
+## Demo
+[Options by default:](https://kotlyarov.us/jquery-mosaic-elements/?default=1)
+Options options with constant height: ([DEMO](https://kotlyarov.us/jquery-mosaic-elements/?ch=1))
+Crazy options: ([DEMO](https://kotlyarov.us/jquery-mosaic-elements/?cp=1))
--- index.php
+++ index.php
@@ -21,6 +21,15 @@ $colors = [
     19 => 'BC8F8F',
     20 => 'ADFF2F'
 ];
+if ($_GET['ch']) {
+    $params = "'masonry': false";
+}
+if ($_GET['co']) {
+    $params = "'rotate_min': -360,";
+    $params .= "'rotate_max': 1800,";
+    $params .= "'duration_min': 300,";
+    $params .= "'duration_max': 1800,";
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -34,32 +43,47 @@ $colors = [
 </head>
 <body>
 
+<? if ($_GET['default']) { ?>
+    <div id="mosaic" class="row">
+        <? for ($i = 0; $i < 60; $i++) { ?>
+            <div href="#" target="_blank" class="mosaic-item col-md-1"
+                 style="background-color: #<?= $colors[rand(1, 20)] ?>; height: <?= rand(150, 300) ?>px">
+                <span class="h1"><?= $i ?></span>
+            </div>
+        <? } ?>
 
-<div id="mosaic" class="row">
-    <? for ($i = 0; $i < 60; $i++) { ?>
-        <div href="#" target="_blank" class="mosaic-item col-md-1"
-             style="background-color: #<?= $colors[rand(1, 20)] ?>; height: <?= rand(150, 300) ?>px">
-            <span class="h1"><?= $i ?></span>
-        </div>
-    <? } ?>
-</div>
+    </div>
+<? } ?>
 
+<? if ($_GET['ch']) { ?>
+    <div id="mosaic" class="row">
+        <? for ($i = 0; $i < 18; $i++) { ?>
+            <div href="#" target="_blank" class="mosaic-item col-md-2"
+                 style="background-color: #<?= $colors[rand(1, 20)] ?>; height:33.33333333333333333333%">
+                <span class="h1"><?= $i ?></span>
+            </div>
+        <? } ?>
+    </div>
+<? } ?>
+
+<? if ($_GET['co']) { ?>
+    <div id="mosaic" class="row">
+        <? for ($i = 0; $i < 60; $i++) { ?>
+            <div href="#" target="_blank" class="mosaic-item col-md-1"
+                 style="background-color: #<?= $colors[rand(1, 20)] ?>; height: <?= rand(60, 600) ?>px">
+                <span class="h1"><?= $i ?></span>
+            </div>
+        <? } ?>
+    </div>
+<? } ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
 <script src="src/jquery.mosaic-elements.js"></script>
-
-
 <script>
     $('body').mosaicElements({
-        // change options
-        // 'rotate_min': -900, // minimum of rotation from in degree
-        // 'rotate_max': 900,
-        // 'duration_min': 180, // minimum of duration from in px
-        // 'duration_max': 900, // maximum of duration to in px
-        //'masonry': false,
+        <?=$params?>
     });
 </script>
-
 
 </body>
 </html>
